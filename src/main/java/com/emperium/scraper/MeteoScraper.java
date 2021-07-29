fbdf8f618278721fc0d8b8f002e7390320d5d024
Stefanos Anastasiou <emperor_stef@yahoo.gr>
2021-07-30 00:16:35 +0300

    adds UnitTests, refactors code, removes DbSetup

--- src/main/java/com/emperium/scraper/MeteoScraper.java
+++ src/main/java/com/emperium/scraper/MeteoScraper.java
@@ -12,6 +12,7 @@ import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.SchedulerException;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
@@ -44,10 +45,13 @@ public class MeteoScraper implements Job {
         startJob();
     }
 
-    public void initScrapingAndSave() {
+    public void initiate() {
         Mappings.cityMappings.keySet()
                 .stream()
-                .forEach(ck -> {
+                .forEach(ck ->initScrapingAndSave(ck));
+    }
+
+    public void initScrapingAndSave(int ck) {
                     try {
                         cityScraper = new CityScraper(ck);
                         cityScraper.scrapeCity();
@@ -55,17 +59,20 @@ public class MeteoScraper implements Job {
                         this.ct = cityScraper.getCity();
 
 
-                        if(cityDAO.cityIsSet(this.ct.getName())) {
+                        if(cityIsSet(this.ct.getName())) {
+//                        if(cityDAO.cityIsSet(this.ct.getName())) {
                             this.ORMDays = new ArrayList<>();
                             this.city_id = cityDAO.getCityId(this.ct.getName());
 
                             ct.days.stream().forEach(domainDay -> {
                                          this.ORMMeasurements = new ArrayList<>();
 
-                                         if(dayDAO.dayIsSet(domainDay.getDate(), this.city_id)) {
+                                if(dayIsSet(domainDay.getDate(), this.city_id)) {
+//                                         if(dayDAO.dayIsSet(domainDay.getDate(), this.city_id)) {
                                              int day_id = dayDAO.getDayId(domainDay.getDate(), this.city_id);
 
-                                             if (measurementsDAO.measurementsAreSet(day_id)) {
+                                         if (dailyMeasurementsAreSet(day_id)) {
+                                             //if (measurementsDAO.measurementsAreSet(day_id)) {
                                                  measurementsDAO.checkAndUpdateDailyMeasurement(day_id, domainDay.measurements);
                                              } else {
                                                  measurementsDAO.setDailyMeasurements(domainDay.measurements, day_id);
@@ -75,7 +82,7 @@ public class MeteoScraper implements Job {
                                               insertRecords(true, domainDay);
                                          }
                             });
-                            deletePreviousMeasurements(city_id);
+                            deletePreviousMeasurements(this.city_id);
                             deletePreviousDays(this.city_id);
                             this.domainDays.clear();
                         } else {
@@ -84,7 +91,18 @@ public class MeteoScraper implements Job {
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
-                });
+    }
+
+    public boolean cityIsSet(String city) {
+        return cityDAO.cityIsSet(city);
+    }
+
+    public boolean dayIsSet(LocalDate date, int city_id) {
+        return dayDAO.dayIsSet(date, city_id);
+    }
+
+    public boolean dailyMeasurementsAreSet(int day_id) {
+        return measurementsDAO.measurementsAreSet(day_id);
     }
 
     private void fillDays(int day_id) {
@@ -160,7 +178,7 @@ public class MeteoScraper implements Job {
                     .forEach(job -> {
                         if (job.getJobDetail().getKey().getName().equals(ScrapeScheduler.SCRAPE_CITY_JOB)) {
                             try {
-                                initScrapingAndSave();
+                                init();
                             } catch (Exception e) {
                                 e.printStackTrace();
                             }
